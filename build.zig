0c2a668c31b20ffefcaa8e59f70f85b8df94eccb
Jimmi Holst Christensen <jhc@dismail.de>
2023-02-16 18:48:50 +0100

    Complete rewrite
    Why? Well, here are some reasons:
    
    - The `pipe` method was an ugly hack that has broken many times
    - The API was not to my liking
    - `ziter` did not compile with latest version of zig
    
    The new codebase is better tested and have more iterators and functions
    that work with iterators. It is inspired by the Rust iterators, but does
    change things up to better fit with how Zig works.

---
+++ build.zig
@@ -0,0 +1,50 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    const test_step = b.step("test", "Run all tests in all modes.");
+    const tests = b.addTest(.{
+        .root_source_file = .{ .path = "ziter.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    test_step.dependOn(&tests.step);
+
+    const readme_step = b.step("readme", "Remake README.");
+    const readme = readMeStep(b);
+    readme.dependOn(test_step);
+    readme_step.dependOn(readme);
+
+    const all_step = b.step("all", "Build everything and runs all tests");
+    all_step.dependOn(test_step);
+    all_step.dependOn(readme_step);
+
+    b.default_step.dependOn(all_step);
+}
+
+fn readMeStep(b: *std.Build) *std.Build.Step {
+    const s = b.allocator.create(std.build.Step) catch unreachable;
+    s.* = std.build.Step.init(.custom, "ReadMeStep", b.allocator, struct {
+        fn make(step: *std.build.Step) anyerror!void {
+            @setEvalBranchQuota(10000);
+
+            const example = try std.mem.replaceOwned(
+                u8,
+                step.dependencies.allocator,
+                @embedFile("example/examples.zig"),
+                "../ziter.zig",
+                "ziter",
+            );
+            defer step.dependencies.allocator.free(example);
+
+            const file = try std.fs.cwd().createFile("README.md", .{});
+            const stream = file.writer();
+            try stream.print(@embedFile("example/README.md.template"), .{
+                example,
+            });
+        }
+    }.make);
+    return s;
+}
