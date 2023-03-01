a0ceb7d12d4849de7f01f8c5effd4680163bdee3
Jimmi Holst Christensen <jhc@dismail.de>
2023-03-01 15:50:38 +0100

    Expose ziter to package manager

--- build.zig
+++ build.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    b.addModule(.{ .name = "ziter", .source_file = .{ .path = "ziter.zig" } });
+
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
