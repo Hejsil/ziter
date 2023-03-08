0c6e30920b338d4d0cc737824e39debc77339968
Jimmi Holst Christensen <jhc@dismail.de>
2023-03-08 16:52:30 +0100

    Update to newest version of zig

--- build.zig
+++ build.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    b.addModule(.{ .name = "ziter", .source_file = .{ .path = "ziter.zig" } });
-
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
+
+    const module = b.addModule("ziter", .{ .source_file = .{ .path = "ziter.zig" } });
 
     const test_step = b.step("test", "Run all tests in all modes.");
     const main_tests = b.addTest(.{
@@ -18,7 +18,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    example_tests.addAnonymousModule("ziter", .{ .source_file = .{ .path = "ziter.zig" } });
+    example_tests.addModule("ziter", module);
 
     test_step.dependOn(&main_tests.step);
     test_step.dependOn(&example_tests.step);
