47053ed658d2074b7ff0776521a2b05d7cccbd82
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2025-05-08 09:08:32 +0200

    refactor: Don't generate README in build.zig

--- build.zig
+++ build.zig
@@ -1,17 +1,15 @@
-const std = @import("std");
-
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const module = b.addModule("ziter", .{ .root_source_file = b.path("ziter.zig") });
-
-    const test_step = b.step("test", "Run all tests in all modes.");
-    const main_tests = b.addTest(.{
+    const module = b.addModule("ziter", .{
         .root_source_file = b.path("ziter.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    const test_step = b.step("test", "Run all tests in all modes.");
+    const main_tests = b.addTest(.{ .root_module = module });
     const run_main_tests = b.addRunArtifact(main_tests);
 
     const example_tests = b.addTest(.{
@@ -25,34 +23,10 @@ pub fn build(b: *std.Build) void {
     test_step.dependOn(&run_main_tests.step);
     test_step.dependOn(&run_example_tests.step);
 
-    const readme_step = b.step("readme", "Remake README.");
-    const readme = readMeStep(b);
-    readme.dependOn(test_step);
-    readme_step.dependOn(readme);
-
     const all_step = b.step("all", "Build everything and runs all tests");
     all_step.dependOn(test_step);
-    all_step.dependOn(readme_step);
 
     b.default_step.dependOn(all_step);
 }
 
-fn readMeStep(b: *std.Build) *std.Build.Step {
-    const s = b.allocator.create(std.Build.Step) catch unreachable;
-    s.* = std.Build.Step.init(.{
-        .id = .custom,
-        .name = "ReadMeStep",
-        .owner = b,
-        .makeFn = struct {
-            fn make(_: *std.Build.Step, _: std.Build.Step.MakeOptions) anyerror!void {
-                @setEvalBranchQuota(10000);
-                const file = try std.fs.cwd().createFile("README.md", .{});
-                const stream = file.writer();
-                try stream.print(@embedFile("example/README.md.template"), .{
-                    @embedFile("example/examples.zig"),
-                });
-            }
-        }.make,
-    });
-    return s;
-}
+const std = @import("std");
